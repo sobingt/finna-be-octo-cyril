d1a3383b4107314adeb57f4a8c41a1f78e39001a
Akshat Verma <v_akshat@yahoo.co.in>
2014-01-16 23:27:28 +0530

    placing the dummy data https://app.asana.com/0/9389190571606/9524717826269

--- import.js
+++ import.js
@@ -3,71 +3,359 @@ module.exports = function (db) {
 
   var deals  =[
   {
-    "_id": "usd49-for-a22-point-winter-furnace-inspection-and-cleaning",
-    "name": "$49 for a 22-Point Winter Furnace Inspection and Cleaning",
+    "_id": "42-percent-off-on-consultation-cleaning-polishing-and-more-at-call-a-dentist",
+    "name": "42% off on Consultation, Cleaning, Polishing and MORE @ Call A Dentist",
     "brand": {
-      "id": 1,
-      "name": "Shoppers Stop",
+      "id": 2,
+      "name": "Call A Dentist",
       "location": {
         "id": "123121",
-        "mall_name": "Inorbit Mall",
+        "mall_name": "Infinity 2",
         "area": "Malad",
         "latitude": 19.173064,
-        "longitude": 72.835616
+        "longitude": 72.835616,
+        "city": "mumbai"
       }
     },
-    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>$49 for a 22-point furnace inspection and cleaning</li>\n<li>Valid for gas furnaces and heat pump</li>\n<li>Valid for one furnace only; purchase additional deals for multiple units</li>\n<li>Cleaning and checking gas burners, pilot assembly, thermocouple, burner flame (adjusting gas pressure as needed), and heat exchanger</li>\n<li>Inspecting flue venting, fan control, limit safety device, electrical wiring, blower speed, amp ddeals, thermostat, heat anticipator setting (from manual override), and belt tension (adjusting tension as needed)</li>\n<li>Changing standard disposable filters (customer provided)</li>\n<li>Confirming overall safe operation</li></ul>",
-    "code": "UX45FG1",
-    "small_image_url": "http://www.shoutbackconcepts.com/admin/upload/cms_pages/5138428b6dd4d.jpg",
-    "big_image_url": "http://www.shoutbackconcepts.com/admin/upload/cms_pages/5138428b6dd4d.jpg",
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>Pay Rs 3499 for three Consultations, Preventive Diagonosis visits.</li>\n<li>Two Teeth Cleanings & Polishings Sittings worth Rs. 6000</li>\n<li>Free of cost X-Rays if required in any visits.</li>\n<li>Flat 25% Discount On Any Treatment After Diagnosis, If patients opts for.</li></ul>",
+    "code": "AG45J90",
+    "small_image_url": "http://media.timesdeal.com/media/deal/2014/Jan/2014011614_228_27023_deal.jpg",
+    "big_image_url": "http://media.timesdeal.com/media/deal/2014/Jan/2014011614_228_27023_deal.jpg",
     "status": "active",
-    "remark": "blah blah blah blah blah",
+    "remark": "Prior appointment is mandatory",
     "category": [
       {
-        "value": "internet"
+        "value": "Health"
       },
       {
-        "value": "reading"
+        "value": "Dental"
       }
     ],
     "created": {
-      "publisher_id": "1231232",
+      "publisher_id": "1431123",
       "start_date": "23-1-2014",
       "end_date": "2-2-2014"
     }
   },
   {
-    "_id": "50-off-on-puma-apparels",
-    "name": "50% off on PUMA Apparels",
+    "_id": "get-65-off-on-loreal-ammonia-free-global-hair-colour-at-blingg-salo",
+    "name": "Get 65% Off on L’Oreal Ammonia Free Global Hair Colour @ Blingg Salo",
     "brand": {
-      "id": 1,
-      "name": "Puma",
+      "id": 3,
+      "name": "Blingg Salo",
       "location": {
-        "id": "123121",
-        "mall_name": "Inorbit Mall",
-        "area": "malad",
-        "latitude": "19.172505000000000000",
-        "longitude": "72.836174000000030000"
+        "id": "122342",
+        "mall_name": "High Street Pheonix",
+        "area": "Lower Parel",
+        "latitude": 19.173064,
+        "longitude": 72.835616,
+        "city": "mumbai"
       }
     },
-    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>$49 for a 22-point furnace inspection and cleaning</li>\n<li>Valid for gas furnaces and heat pump</li>\n<li>Valid for one furnace only; purchase additional deals for multiple units</li>\n<li>Cleaning and checking gas burners, pilot assembly, thermocouple, burner flame (adjusting gas pressure as needed), and heat exchanger</li>\n<li>Inspecting flue venting, fan control, limit safety device, electrical wiring, blower speed, amp ddeals, thermostat, heat anticipator setting (from manual override), and belt tension (adjusting tension as needed)</li>\n<li>Changing standard disposable filters (customer provided)</li>\n<li>Confirming overall safe operation</li></ul>",
-    "code": "UX45FG1",
-    "small_image_url": "http://www.shoutbackconcepts.com/admin/upload/cms_pages/5138428b6dd4d.jpg",
-    "big_image_url": "http://www.shoutbackconcepts.com/admin/upload/cms_pages/5138428b6dd4d.jpg",
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>L'Oreal Ammonia Free Global Hair Color Valid upto mid length</li>\n<li>Valid for only females</li>\n<li>Carrying print out/SMS of the voucher is mandatory to avail the offer</li>\n<li>Multiple vouchers can be purchased for gifting</li>\n<li>Valid on all days</li>\n<li>Providing the voucher code is mandatory before availing the offer</li>\n<li>Taxes Included</li></ul>",
+    "code": "GH565AS1",
+    "small_image_url": "http://media.timesdeal.com/media/deal/2014/Jan/2014011517_228_27041_deal.jpg",
+    "big_image_url": "http://media.timesdeal.com/media/deal/2014/Jan/2014011517_228_27041_deal.jpg",
     "status": "active",
-    "remark": "blah blah blah blah blah",
+    "remark": "Valid till 15th Feb",
     "category": [
       {
-        "value": "internet"
+        "value": "Beauty"
       },
       {
-        "value": "reading"
+        "value": "Spa & Salon"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231232",
+      "start_date": "20-1-2014",
+      "end_date": "15-2-2014"
+    }
+  },
+  {
+    "_id": "enjoy-upto-20-off-on-lunch-and-dinner-at-rs-19",
+    "name": "Enjoy upto 20% off on lunch & dinner at Rs 19",
+    "brand": {
+      "id": 4,
+      "name": "Dakshin Culture Curry",
+      "location": {
+        "id": "1231231",
+        "mall_name": "R-City",
+        "area": "Ghatkopar",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>20% off on lunch, 15% off on dinner</li>\n<li>Multi-cuisine restaurant</li>\n<li>1 voucher per bill</li>\n<li>Valid for dine in only</li>\n<li>This offer cannot be clubbed with other promotions</li>\n<li>Authentic South Indian delicacies</li></ul>",
+    "code": "AQ12Vb67",
+    "small_image_url": "http://images.mydala.com/uploads/event/2013-12-13/110664/110664_1_deal.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2013-12-13/110664/110664_1_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 20 Feb",
+    "category": [
+      {
+        "value": "Food"
+      },
+      {
+        "value": "restaurants"
+      },
+      {
+        "value": "Dining"
       }
     ],
     "created": {
       "publisher_id": "1231233",
-      "start_date": "23-1-2014",
-      "end_date": "2-2-2014"
+      "start_date": "15-1-2014",
+      "end_date": "20-2-2014"
+    }
+  },
+  {
+    "_id": "celebrate-goa-the-mumbaikar-way-at-rs-19",
+    "name": "Celebrate Goa the Mumbaikar way at Rs 19",
+    "brand": {
+      "id": 4,
+      "name": "Goa Portuguesa",
+      "location": {
+        "id": "1231231",
+        "mall_name": "R-City",
+        "area": "Ghatkopar",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>20% off on lunch, 15% off on dinner</li>\n<li>Multi-cuisine restaurant</li>\n<li>Six times winner of the Best Restaurant Award by the Times Food Guide, TOI Group</li>\n<li>Valid for dine in only</li>\n<li>This offer cannot be clubbed with other promotions</li>\n<li>India's 1st ISO 9001 Certified Restobar</li></ul>",
+    "code": "AQ12Vb67",
+    "small_image_url": "http://images.mydala.com/uploads/event/2013-12-12/110486/110486_1_deal.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2013-12-12/110486/110486_1_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 20 Feb",
+    "category": [
+      {
+        "value": "Food"
+      },
+      {
+        "value": "restaurants"
+      },
+      {
+        "value": "Dining"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "20-2-2014"
+    }
+  },
+  {
+    "_id": "30-off-on-room-tariff-in-hyderabad",
+    "name": "30% Off on Room Tariff in Hyderabad",
+    "brand": {
+      "id": 1,
+      "name": "Birbal Travels",
+      "location": {
+        "id": "1231231",
+        "mall_name": "Inorbit Mall",
+        "area": "Malad",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>30% off on room tariff in Hyderabad</li>\n<li>Popular 3 Star Hotel</li>\n<li>Rooms equipped with modern amenities</li>\n<li>Simple booking procedure & Room Tariff</li>\n<li>India's 1st ISO 9001 Certified Travel company</li></ul>",
+    "code": "AQ12Vb67",
+    "small_image_url": "http://images.mydala.com/uploads/event/2013-12-30/112711/112711_1_deal.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2013-12-30/112711/112711_1_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 20 Feb",
+    "category": [
+      {
+        "value": "Hotels"
+      },
+      {
+        "value": "Travel"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "20-2-2014"
+    }
+  },
+  {
+    "_id": "additional-50-off-on-already-discounted-apparel-&-accessories",
+    "name": "Additional 50% off on already discounted apparel & accessories",
+    "brand": {
+      "id": 1,
+      "name": "Brand Trendz",
+      "location": {
+        "id": "1236542",
+        "mall_name": "Raghuleela",
+        "area": "Borivali",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>50% off on Discounted apparells</li>\n<li>Popular 3 brands available</li>\n<li>Valid till 6th Feb</li>\n<li>Simple availing procedure </li></ul>",
+    "code": "BTMD50",
+    "small_image_url": "http://images.mydala.com/uploads/event/2014-01-10/114214/114214_1_homecat.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2014-01-10/114214/114214_1_homecat.jpg",
+    "status": "active",
+    "remark": "Valid till 6 Feb",
+    "category": [
+      {
+        "value": "apparels"
+      },
+      {
+        "value": "shopping"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "20-2-2014"
+    }
+  },
+  {
+    "_id": "enjoy-buy-1-get-1-offer-on-games-at-rs-29",
+    "name": "Enjoy Buy 1 Get 1 Offer on Games at Rs. 29",
+    "brand": {
+      "id": 1,
+      "name": "Play 360 Game Lounge",
+      "location": {
+        "id": "122342",
+        "mall_name": "High Street Pheonix",
+        "area": "Lower Parel",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>Offer Includes:buy 1 get 1 offer on games (for 2 people)</li>\n<li>Choose from a variety of games(Assasins Creed, Hitman, Angry Birds, FIFA, Resident Evil and more)</li>\n<li>One of the leading gaming lounge</li>\n<li>Applicable taxes included </li></ul>",
+    "code": "BTMD50",
+    "small_image_url": "http://images.mydala.com/uploads/event/2013-04-08/82071/82071_1_deal.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2013-04-08/82071/82071_1_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 21 Feb",
+    "category": [
+      {
+        "value": "gaming"
+      },
+      {
+        "value": "Activities & Hobbies"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "21-2-2014"
+    }
+  },
+  {
+    "_id": "buy-1-get-1-offer-on-9D-movie-tickets",
+    "name": "Buy 1 get 1 offer on 9D movie tickets",
+    "brand": {
+      "id": 1,
+      "name": "Max 9D Interactive Cinegames",
+      "location": {
+        "id": "121231",
+        "mall_name": "Inorbit Mall",
+        "area": "Malad",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>Buy 1 get 1 offer on 9D movie tickets</li>\n<li>Experience a new level of entertainment</li>\n<li>Korean technology</li>\n<li>Applicable taxes included </li></ul>",
+    "code": "BTMD50",
+    "small_image_url": "http://images.mydala.com/uploads/event/2014-01-02/113025/113025_1_deal.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2014-01-02/113025/113025_1_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 21 Feb",
+    "category": [
+      {
+        "value": "gaming"
+      },
+      {
+        "value": "Activities & Hobbies"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "21-2-2014"
+    }
+  },
+  {
+    "_id": " book-an-appointment-to-get-free-eye-test",
+    "name": " Book an appointment to get Free eye test",
+    "brand": {
+      "id": 21,
+      "name": "Vision Express",
+      "location": {
+        "id": "122231",
+        "mall_name": "Hypercity Mall",
+        "area": "Kurla",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>Free eye test</li>\n<li>Comprehensive 7 step eye test</li>\n<li>Expert optometristsy</li>\n<li>State-of-the-art clinic</li></ul>",
+    "code": "BTMD50",
+    "small_image_url": "http://images.mydala.com/uploads/event/2013-12-03/108724/108724_1_deal.jpg",
+    "big_image_url": "http://images.mydala.com/uploads/event/2013-12-03/108724/108724_1_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 21 Feb",
+    "category": [
+      {
+        "value": "Health Care"
+      },
+      {
+        "value": "Utilities"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "21-2-2014"
+    }
+  },
+  {
+    "_id": "50-off-on-bowling-games,-pool-game-and-more",
+    "name": "50% off on Bowling Games, Pool Game & MORE",
+    "brand": {
+      "id": 19,
+      "name": "The Acres Club",
+      "location": {
+        "id": "122231",
+        "mall_name": "Hypercity Mall",
+        "area": "Kurla",
+        "latitude": "19.172505000000000000",
+        "longitude": "72.836174000000030000",
+        "city": "mumbai"
+      }
+    },
+    "description": "<ul class=\"storefront\" style=\"list-style-type:disc\">\n<li>Pay Rs 375 for 2 Bowling Games</li>\n<li>1/2 an Hour Pool Game</li>\n<li>1/2 an Hour Soft Play</li>\n<li>10 Video Games Coins worth Rs 750</li></ul>",
+    "code": "BTMD50",
+    "small_image_url": "http://media.timesdeal.com/media/deal/2013/Dec/2013120516_228_25643_deal.jpg",
+    "big_image_url": "http://media.timesdeal.com/media/deal/2013/Dec/2013120516_228_25643_deal.jpg",
+    "status": "active",
+    "remark": "Valid till 21 Feb",
+    "category": [
+      {
+        "value": "Gaming"
+      },
+      {
+        "value": "Activities & Hobbies"
+      }
+    ],
+    "created": {
+      "publisher_id": "1231233",
+      "start_date": "15-1-2014",
+      "end_date": "21-2-2014"
     }
   }
 ];
